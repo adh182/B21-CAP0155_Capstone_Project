32108f52c69cc64b57ad834bb8cd41c31eaa6344
Adh <adh.isl.almrjl@gmail.com>
2021-05-08 11:22:19 +0800

    Add commas

--- gcp-config/deployment-manager/network/firewall-template.py
+++ gcp-config/deployment-manager/network/firewall-template.py
@@ -7,10 +7,10 @@ def GenerateConfig(context):
       'name': context.env['name']+'allow-http',
       'type': 'compute.v1.firewall',
       'properties': {
-          'network': context.properties['network']
-          'sourceRanges': ['0.0.0.0/0']
+          'network': context.properties['network'],
+          'sourceRanges': ['0.0.0.0/0'],
           'allowed': [{
-              'IPProtocol': 'HTTP'
+              'IPProtocol': 'HTTP',
               'ports': ["80"]
           }]
       }
--- gcp-config/deployment-manager/network/subnetwork-template.py
+++ gcp-config/deployment-manager/network/subnetwork-template.py
@@ -7,8 +7,8 @@ def GenerateConfig(context):
       'name': context.env['name'],
       'type': 'compute.v1.subnetwork',
       'properties': {
-          'ipCidrRange': context.properties['ipCidrRange']
-          'network': context.properties['network']
+          'ipCidrRange': context.properties['ipCidrRange'],
+          'network': context.properties['network'],
           'region': context.properties['region']
       }
   }]

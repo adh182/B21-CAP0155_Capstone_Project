f1c0410eba13eb4aabd011b6a2d23e1c73e3ca43
Adh <adh.isl.almrjl@gmail.com>
2021-05-08 14:20:44 +0800

    Add bucket template

---
+++ gcp-config/deployment-manager/storage/bucket-template.py
@@ -0,0 +1,22 @@
+#Storage Bucket Template
+
+def GenerateConfig(context):
+	'''Cretae cloud storage bucket'''
+
+	PROJECT_NAME = 'B21-CAP0155-Capstone-Project'
+	
+	resources = [{
+		'name': PROJECT_NAME + context.env['name'],
+		'type': 'storage.v1.bucket',
+		'properties': {
+			'locationType': 'MULTI-REGION',
+			'location': 'ASIA',
+			'storageClass': 'STANDARD',
+			'versioning': {
+				'enabled': True
+			}
+		}
+
+	}]
+
+return {'resources': resources}

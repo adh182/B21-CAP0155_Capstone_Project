034a1c61a7d309c9929a511d2a03585426cbe1a2
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2021-05-20 15:38:03 +0700

    Update datasetSerializer.py

--- Capstone-ml/django_rest/api/serializers/datasetSerializer.py
+++ Capstone-ml/django_rest/api/serializers/datasetSerializer.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class DatasetSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Dataset
-		fields = '__all__' #menampilkan semua field pada class Dataset
+		fields = '__all__'

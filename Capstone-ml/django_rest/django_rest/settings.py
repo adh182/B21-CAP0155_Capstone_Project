e3a9c03f0ef8816e146e4af243ea30fa246a356c
Islamuddin Alimurrijal <adh.isl.almrjl@gmail.com>
2021-05-23 09:36:27 +0800

    Add STATIC_ROOT

--- Capstone-ml/django_rest/django_rest/settings.py
+++ Capstone-ml/django_rest/django_rest/settings.py
@@ -137,7 +137,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Default primary key field type

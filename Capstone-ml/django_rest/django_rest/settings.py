de6657740157fcf4c463b108e91128c9bdc05dec
Islamuddin Alimurrijal <adh.isl.almrjl@gmail.com>
2021-05-25 20:20:59 +0800

    Update settings.py

--- Capstone-ml/django_rest/django_rest/settings.py
+++ Capstone-ml/django_rest/django_rest/settings.py
@@ -137,9 +137,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATIC_URL = '/static/'
+# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# STATIC_URL = '/static/'
+STATIC_URL = 'https://storage.googleapis.com/b21-cap0155-capstone-project.appspot.com/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
